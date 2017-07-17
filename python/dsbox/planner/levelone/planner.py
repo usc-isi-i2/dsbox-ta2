310cec614505aa0b1cbc31933481e98b0fd1442b
Ke-Thia Yao <kyao@isi.edu>
2017-07-17 16:38:59 -0700

    Add d3m primitive json processing

--- python/dsbox/planner/levelone/planner.py
+++ python/dsbox/planner/levelone/planner.py
@@ -26,6 +26,57 @@ class Ontology(object):
         """Returns task names"""
         return [node["Name"] for node in self.task]
 
+class Primitive(object):
+    """A primitive"""
+    def __init__(self, definition):
+        self.name = definition['Name']
+        self.task = definition['Task']
+        self.learning_type = definition['LearningType']
+        self.ml_algorithm = definition['MachineLearningAlgorithm']
+
+    def __str__(self):
+        return 'Primitive({})'.format(self.name)
+
+    def __eq__(self, other):
+        """Define equals based on name"""
+        if isinstance(other, self.__class__):
+            return self.name == other.name
+        return NotImplemented
+
+    def __ne__(self, other):
+        """Overide non-equality test"""
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
+
+    def __hash__(self):
+        """Overide hash by using name attribute"""
+        return hash(self.name)
+
+class D3Primitive(Primitive):
+    def __init__(self, definition):
+        self.name = definition['common_name']
+
+        self.task = ''
+        if 'task_type' in definition:
+            if 'feature extraction' in definition['task_type']:
+                self.task = "FeatureExtraction"
+            if 'data preprocessing' in definition['task_type']:
+                self.task = "DataPreprocessing"
+
+        self.learning_type = 'NA'
+        if 'handles_classification' in definition and definition['handles_classification']:
+            self.learning_type = 'Classification'
+        if 'handles_regression' in definition and definition['handles_regression']:
+            self.learning_type = 'Regression'
+
+        if 'algorithm_type' in definition:
+            # !!!! For now get only the first type
+            self.ml_algorithm = definition['algorithm_type'][0]
+        else:
+            self.ml_algorithm = 'NA'
+
+
 class Primitives(object):
     """Maintain available primitives"""
     PRIMITIVE_FILE = 'primitives.json'
@@ -34,36 +85,36 @@ class Primitives(object):
         self._load()
         self._index = dict()
         for index, primitive in enumerate(self.primitives):
-            self._index[primitive['Name']] = index
+            self._index[primitive.name] = index
         self.size = len(self.primitives)
 
     def _load(self):
         """Load primitive definition from JSON file"""
         text = pkgutil.get_data('dsbox.planner.levelone', self.PRIMITIVE_FILE)
         content = json.loads(text.decode())
-        self.primitives = content['Primitives']
+        self.primitives = [Primitive(primitive_dict) for primitive_dict in content['Primitives']]
 
     def filter_equality(self, aspect, name):
         """Find primitive by aspect and name value"""
-        result = [p for p in self.primitives if p[aspect] == name]
+        result = [p for p in self.primitives if getattr(p, aspect) == name]
         return result
 
     def filter_by_task(self, name):
         """Find primitive by task aspect and name value"""
-        return self.filter_equality('Task', name)
+        return self.filter_equality('task', name)
 
     def filter_by_learning_type(self, name):
         """Find primitive by learning-type aspect and name value"""
-        return self.filter_equality('LearningType', name)
+        return self.filter_equality('learning_type', name)
 
     def filter_by_algo(self, name):
         """Find primitive by algorithm aspect and name value"""
-        return self.filter_equality('MachineLearningAlgorithm', name)
+        return self.filter_equality('ml_algorithm', name)
 
     def get_by_name(self, name):
         """Get primitve by unique name"""
         for primitive in self.primitives:
-            if primitive['Name'] == name:
+            if primitive.name == name:
                 return primitive
         return None
 
@@ -71,35 +122,58 @@ class Primitives(object):
         """Returns the index of the primitive given its name"""
         return self._index[name]
 
+#    def get_supervised_learning_by_algorithm(self):
+#        for learning_type in ['Classification', 'Regression']:
+
+class D3Primitives(Primitives):
+    PRIMITIVE_FILE = 'sklearn.json'
+    def __init__(self):
+        self._load()
+        self._index = dict()
+        for index, primitive in enumerate(self.primitives):
+            self._index[primitive.name] = index
+        self.size = len(self.primitives)
+
+    def _load(self):
+        text = pkgutil.get_data('dsbox.planner.levelone', self.PRIMITIVE_FILE)
+        content = json.loads(text.decode())
+        self.primitives = [D3Primitive(primitive_dict) for primitive_dict in content['search_primitives']]
+
 class ConfigurationSpace(object):
-    """Defines the space of pipelines"""
+    """Defines the space of primitive pipelines"""
 
     def __init__(self, dimension_names, space):
         assert len(dimension_names) == len(space)
+        self.ndim = len(dimension_names)
         self.dimension_names = dimension_names
         self.space = space
+        self._subspace_lookup = dict()
+        for name, subspace in zip(dimension_names, space):
+            self._subspace_lookup[name] = subspace
 
-    def get_random_configuration(self, seed=None):
+    @classmethod
+    def set_seed(cls, a_seed):
+        """Set random seed"""
+        random.seed(a_seed)
+
+    def get_random_configuration(self):
         """Returns a random configuration from the configuration space"""
-        if seed:
-            random.seed(seed)
         components = []
         for component_space in self.space:
             i = random.randrange(len(component_space))
             component = component_space[i]
             components.append(component)
-        return components
+        return ConfigurationPoint(self, components)
 
     def get_configuration_by_policy(self, policy):
         """Returns a random configuration based on stochastic policy"""
         components = []
         component_names = []
         for component_space in self.space:
-            names = [comp['Name'] for comp in component_space]
+            names = [p.name for p in component_space]
             if components:
-                values = np.power(10,
-                                  policy.get_affinities(component_names, names))
-                rand = random.randrange(np.sum(values))
+                values = policy.get_affinities(component_names, names)
+                rand = np.sum(values) * random.random()
                 i = 0
                 while rand > values[i]:
                     rand -= values[i]
@@ -108,15 +182,35 @@ class ConfigurationSpace(object):
                 i = random.randrange(len(component_space))
             component = component_space[i]
             components.append(component)
-            component_names.append(component['Name'])
-        return components
+            component_names.append(component.name)
+        return ConfigurationPoint(self, components)
+
+class ConfigurationPoint(object):
+    """A point in ConfigurationSpace"""
+    def __init__(self, configuration_space, point=None):
+        self.configuration_space = configuration_space
+        if point is None:
+            self.point = configuration_space.ndim * [None]
+        else:
+            self.set_point(point)
+
+    def set_point(self, point):
+        """set point value list"""
+        assert len(point) == self.configuration_space.ndim
+        for dim, subspace in zip(point, self.configuration_space.space):
+            if dim is not None:
+                assert dim in subspace
+        self.point = point
 
 
 class AffinityPolicy(object):
-    """Defines affinity pairs of components"""
+    """Defines affinity pairs of components.
+    The default affinity is 1. If affinity value is 10 then the pair
+    is 10x more likely to occur together.
+    """
     def __init__(self, primitives):
         self.primitives = primitives
-        self.affinity_matrix = np.zeros((primitives.size, primitives.size))
+        self.affinity_matrix = np.ones((primitives.size, primitives.size))
 
     def set_affinity(self, source_primitive, dest_primitive, affinity_value):
         """Set affinity between source to destination primitive"""
@@ -152,8 +246,8 @@ class AffinityPolicy(object):
 
 class Pipeline(object):
     """Defines a sequence of executions"""
-    def __init__(self, component_list):
-        self.component_list = component_list
+    def __init__(self, configuration_point):
+        self.configuration_point = configuration_point
 
     @classmethod
     def get_random_pipeline(cls, configuration_space):
@@ -161,7 +255,14 @@ class Pipeline(object):
         return Pipeline(configuration_space.get_random_configuration())
 
     def __str__(self):
-        return 'Pipeline(' + ','.join([c['Name'] for c in self.component_list]) + ')'
+        out_list = []
+        for name, c in zip(self.configuration_point.configuration_space.dimension_names,
+                           self.configuration_point.point):
+            if c is None:
+                out_list.append('{}=None'.format(name))
+            else:
+                out_list.append('{}={}'.format(name,c.name))
+        return 'Pipeline(' + ', '.join(out_list) + ')'
 
 class LevelOnePlanner(object):
     """Level One Planner"""
@@ -251,12 +352,22 @@ def main():
     planner = LevelOnePlanner(primitives=primitives)
     policy = AffinityPolicy(primitives)
 
-    policy.set_symetric_affinity('Descritization', 'NaiveBayes', 1)
-    policy.set_symetric_affinity('Normalization', 'SVM', 1)
+    policy.set_symetric_affinity('Descritization', 'NaiveBayes', 10)
+    policy.set_symetric_affinity('Normalization', 'SVM', 10)
 
 
     pipelines = planner.generate_pipelines_with_policy(policy, 20,
-                                                       ignore_preprocessing=False)
+                                                       ignore_preprocessing=True)
+    for pipeline in pipelines:
+        print(pipeline)
+
+def testd3():
+    """Test method"""
+    primitives = D3Primitives()
+    planner = LevelOnePlanner(primitives=primitives)
+    policy = AffinityPolicy(primitives)
+
+    pipelines = planner.generate_pipelines(20, ignore_preprocessing=True)
     for pipeline in pipelines:
         print(pipeline)
 
