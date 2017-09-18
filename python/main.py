576db2eef3d37292272b50c0709c2574098defcd
Daragh Hartnett <hartnett@ai.sri.com>
2017-09-18 14:32:19 -0700

    Updated and first verified working version of the entry point for the NIST TA2 evaluation system. See "TA2 Evaluation section in the README.md"

--- python/main.py
+++ python/main.py
@@ -15,7 +15,7 @@ import os
 #    https://datadrivendiscovery.org/wiki/pages/viewpage.action?spaceKey=gov&title=TA2+Submission+Guide
 ####################################################################################################################
 
-## Grab the input parameters
+## Grab the input parameters passed in on the docker command and display them
 print "main.py invoked to perform", sys.argv[1]
 print "main.py invoked with config file", sys.argv[2]
 
@@ -23,10 +23,25 @@ print "main.py invoked with config file", sys.argv[2]
 with open(sys.argv[2]) as data_file:
     data = json.load(data_file)
 
-problem = data["problem_schema"]
-output = data["executables_root"]
+## Convert the provided parameters into the form that DSBox wants
+problem = data["problem_schema"][ : data["problem_schema"].find("problemSchema.json")]
+output = data["executables_root"][ : data["executables_root"].find("executables")]
+
+print "problem directory is: ", problem
+print "output directory is: ", output
+
+## TODO: Chnage the way DSBox uses paths and file names to match the provided government format - this will prevent failures in case they make changes
+## Here is a sample config.json:
+# {
+#     "problem_schema": "/baseball/problemSchema.json",
+#     "dataset_schema": "/baseball/data/dataSchema.json",
+#     "training_data_root": "/baseball/data",
+#     "pipeline_logs_root": "/outputs/logs",
+#     "executables_root": "/outputs/executables",
+#     "temp_storage_root": "/temp"
+# }
+
 
 ## Call the ISI TA2 System with the provided parameters
-# python run.py -p ../data/o_38 -l library -o outputs/o_38
 os.system("python run.py -p " + problem + " -l library -o " + output)
 
