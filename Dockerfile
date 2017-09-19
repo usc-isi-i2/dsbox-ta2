226cf3222183969b5459b42b319d67432bf1107f
Daragh Hartnett <hartnett@ai.sri.com>
2017-09-19 09:45:00 -0700

    Docker files & NIST required scripts to support the TA2 Evaluation pipeline. The image is based on the JPL provided python 3.5 image.

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM registry.datadrivendiscovery.org/jpl/docker_images/jpl_base_python3.5
+MAINTAINER D3M
+
+
+RUN \
+pip3 install punk && \
+pip3 install langdetect && \
+pip3 install zerorpc && \
+pip3 install stopit
+
+EXPOSE 8888
+
+#COPY start.sh /home/start.sh
+#RUN set -ex \ && chmod 755 /home/start.sh
+#WORKDIR /home
+#CMD ["./start.sh"]
+
+# Copy the TA2 code to the docker container
+ENV CODE /home
+COPY python $CODE
+
+# work-around for non-interactive shells
+RUN echo '#!/bin/bash\npython $CODE/main.py search "$@"' > /usr/bin/ta2_search
+RUN chmod +x /usr/bin/ta2_search
---
+++ python/main.py
@@ -0,0 +1,47 @@
+import sys
+import json
+import os
+
+####################################################################################################################
+# Entry point for the TA2 Evaluation system. This script will be aliased to the command ta2_search in the docker
+# image and called when the container is run:
+#
+#    docker run -i --entrypoint /bin/bash <container_id> -c 'ta2_search $CONFIG_JSON_PATH'
+#
+# The $CONFIG_JSON_PATH contains the path to the problem and data as well as the output path
+#
+# See the following for more details:
+#
+#    https://datadrivendiscovery.org/wiki/pages/viewpage.action?spaceKey=gov&title=TA2+Submission+Guide
+####################################################################################################################
+
+## Grab the input parameters passed in on the docker command and display them
+print "main.py invoked to perform", sys.argv[1]
+print "main.py invoked with config file", sys.argv[2]
+
+## Parse the input JSON config file
+with open(sys.argv[2]) as data_file:
+    data = json.load(data_file)
+
+## Convert the provided parameters into the form that DSBox wants
+problem = data["problem_schema"][ : data["problem_schema"].find("problemSchema.json")]
+output = data["executables_root"][ : data["executables_root"].find("executables")]
+
+print "problem directory is: ", problem
+print "output directory is: ", output
+
+## TODO: Chnage the way DSBox uses paths and file names to match the provided government format - this will prevent failures in case they make changes
+## Here is a sample config.json:
+# {
+#     "problem_schema": "/baseball/problemSchema.json",
+#     "dataset_schema": "/baseball/data/dataSchema.json",
+#     "training_data_root": "/baseball/data",
+#     "pipeline_logs_root": "/outputs/logs",
+#     "executables_root": "/outputs/executables",
+#     "temp_storage_root": "/temp"
+# }
+
+
+## Call the ISI TA2 System with the provided parameters
+os.system("python3 run.py -p " + problem + " -l library -o " + output)
+
