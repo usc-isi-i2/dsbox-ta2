dc141ac60c00b894ea4c48ec8d95293508c82afe
Robert Brekelmans <brekelma@dsbox01.isi.edu>
2018-02-01 16:50:11 -0800

    Revert "Merge branch 'ensemble_rob' of https://github.com/usc-isi-i2/dsbox-ta1" merge of ensemble_rob branch done by mistake
    Thig reverts commit 5fb517bca3d3b7e904c418e6007f98c2c30b9894, reversing
    changes made to 876866a909d5d7b198e53c602fd6fbb77686bb51.

--- python/dsbox/planner/controller.py
+++ python/dsbox/planner/controller.py
@@ -19,7 +19,6 @@ from dsbox.planner.common.data_manager import Dataset, DataManager
 from dsbox.planner.common.pipeline import Pipeline, PipelineExecutionResult
 from dsbox.planner.common.problem_manager import Problem
 from dsbox.planner.common.resource_manager import ResourceManager
-from dsbox.planner.ensemble import Ensemble
 
 class Feature:
     def __init__(self, resource_id, feature_name):
@@ -36,7 +35,6 @@ class Controller(object):
     num_cpus = 0
     ram = 0
     timeout = 60
-    ensemble_add = 4
 
     exec_pipelines = []
     l1_planner = None
@@ -65,8 +63,6 @@ class Controller(object):
         self.ram = config.get('ram', 0)
         self.timeout = (config.get('timeout', 60))*60
 
-        self.ensemble_add = config.get('ensemble_add', 0)
-
         # Create some debugging files
         self.logfile = open("%s%slog.txt" % (self.tmp_dir, os.sep), 'w')
         self.errorfile = open("%s%sstderr.txt" % (self.tmp_dir, os.sep), 'w')
@@ -83,7 +79,7 @@ class Controller(object):
     '''
     Set config directories and schema from just problemdir, datadir and outputdir
     '''
-    def initialize_simple(self, problemdir, datadir, outputdir, ensemble_add = 4):
+    def initialize_simple(self, problemdir, datadir, outputdir):
         self.initialize_from_config({
             "problem_root": problemdir,
             "problem_schema": problemdir + os.sep + 'problemDoc.json',
@@ -94,8 +90,7 @@ class Controller(object):
             'temp_storage_root': outputdir + os.sep + "temp",
             "timeout": 60,
             "cpus"  : "4",
-            "ram"   : "4Gi",
-            "ensemble_add": ensemble_add
+            "ram"   : "4Gi"
         })
 
     """
@@ -157,13 +152,10 @@ class Controller(object):
     """
     Train and select pipelines
     """
-    def train(self, planner_event_handler, cutoff=10, ensemble = True):
+    def train(self, planner_event_handler, cutoff=10):
         self.exec_pipelines = []
         self.l2_planner.primitive_cache = {}
         self.l2_planner.execution_cache = {}
-
-        if ensemble:
-            self.ensemble = Ensemble(self.problem)
 
         self.logfile.write("Task type: %s\n" % self.problem.task_type)
         self.logfile.write("Metrics: %s\n" % self.problem.metrics)
@@ -219,7 +211,6 @@ class Controller(object):
                 yield pe.RunningPipeline(l2_pipeline)
                 # exec_pipeline = self.l2_planner.patch_and_execute_pipeline(l2_pipeline, df, df_lbl)
 
-            
             exec_pipelines = self.resource_manager.execute_pipelines(l2_pipelines, df, df_lbl)
             for exec_pipeline in exec_pipelines:
                 l2_pipeline = l2_pipelines_map[str(exec_pipeline)]
@@ -227,7 +218,6 @@ class Controller(object):
                 yield pe.CompletedPipeline(l2_pipeline, exec_pipeline)
                 if exec_pipeline:
                     self.exec_pipelines.append(exec_pipeline)
-
 
             self.exec_pipelines = sorted(self.exec_pipelines, key=lambda x: self._sort_by_metric(x))
             self.logfile.write("\nL2 Executed Pipelines:\n-------------\n")
@@ -251,11 +241,6 @@ class Controller(object):
             self.logfile.write("%s\n" % str(l1_related_pipelines))
             l1_pipelines = l1_related_pipelines
 
-            self.ensemble.greedy_add(self.exec_pipelines, df, df_lbl, pipelines_to_add = self.ensemble_add)
-            self.logfile.write("\nEnsemble Pipelines:\n-------------\n")
-            for a in self.ensemble.pipelines:
-                self.logfile.write("%s\n" % a)
-
         self.write_training_results()
 
     '''
@@ -270,18 +255,6 @@ class Controller(object):
 
         # Create executables
         self.pipelinesfile.write("# Pipelines ranked by metrics (%s)\n" % self.problem.metrics)
-        
-        # Write ensemble results
-        metric_values = []
-        
-        for metric in self.ensemble.metric_values.keys():
-            metric_value = self.ensemble.metric_values[metric]
-            metric_values.append("%s = %2.4f" % (metric, metric_value))
-
-            self.pipelinesfile.write("%s ( %s ) : %s\n" % ('ensemble', self.ensemble.pipelines, metric_values))
-            #self.execution_helper.create_pipeline_executable(pipeline, self.config)
-            #self.create_pipeline_logfile(pipeline, rank)
-
         for index in range(0, len(self.exec_pipelines)):
             pipeline = self.exec_pipelines[index]
             rank = index + 1
