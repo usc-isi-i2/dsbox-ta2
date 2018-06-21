af09eed005f44a6db9a224c64baee4e913ebbbd2
Ehsan <qasemi@isi.edu>
2018-06-21 11:14:02 -0700

    change the way we print ooutcome of each configuration point evaluation to be more compact. In this version the hyperparamters and primitive are printed as well as all the output metrics from evaluation phase

--- python/dsbox/template/pipeline_utilities.py
+++ python/dsbox/template/pipeline_utilities.py
@@ -9,5 +9,5 @@ import os
 
 def pipe2str(pipeline: Pipeline) -> typing.AnyStr:
     # TODO
-    desc = list(map(lambda s: (s.primitive,s.hyperparams) , pipeline.steps))
+    desc = list(map(lambda s: (s.primitive, s.hyperparams), pipeline.steps))
     return pprint.pformat(desc)
