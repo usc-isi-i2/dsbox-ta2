9a6bea9698f4f1e7203a242d3450549abf89ef0e
Serban Stan <serbanandrei.stan@gmail.com>
2018-07-17 15:55:16 -0700

    Update train_test.py

--- python/train_test.py
+++ python/train_test.py
@@ -1,17 +1,25 @@
 import os
+import subprocess
+
+from multiprocessing.pool import ThreadPool
 from pathlib import Path
 
-import subprocess
+def call_ta2search(command):
+	p = subprocess.Popen(command, shell=True)
+	p.communicate()
+
+num = 5
+tp = ThreadPool(num)
 
 home = str(Path.home())
 config_dir = home + "/dsbox/runs2/config-ll0/"
 
 for conf in os.listdir(config_dir):
 	command = "python ta2-search " + config_dir + conf
+
 	print(command)
-	subprocess.Popen(command, shell=True)
 
+	tp.apply_async(call_ta2search, (command,))
 
-# TODO: add rerun utility for nonexistent text files
-
-# TODO: do a more elegant subprocess management - one process finishes start new process
+tp.close()
+tp.join()
