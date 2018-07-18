4c1cb042aa1185f5d411904a3fd996caaf789a1b
serbanstan <serbanandrei.stan@gmail.com>
2018-07-17 21:18:21 -0700

    commented out new code in train_test

--- python/train_test.py
+++ python/train_test.py
@@ -7,6 +7,9 @@ from pathlib import Path
 
 
 def call_ta2search(command):
+    print(command)
+    print('\n')
+
     p = subprocess.Popen(command, shell=True)
     p.communicate()
 
@@ -15,13 +18,12 @@ num = 5
 tp = ThreadPool(num)
 
 home = str(Path.home())
-config_dir = sys.argv[2]
+# config_dir = sys.argv[2]
+config_dir = home + "/dsbox/runs2/config-ll0/"
 
 for conf in os.listdir(config_dir):
-    command = "python3 ta1-run-single-template --template " + sys.argv[1] + " " + \
-              os.path.join(config_dir, conf, 'search_config.json')
-
-    print(command)
+    # command = "python3 ta1-run-single-template --template " + sys.argv[1] + " " + os.path.join(config_dir, conf, 'search_config.json')
+    command = "python ta2-search " + config_dir + conf
 
     tp.apply_async(call_ta2search, (command,))
 
