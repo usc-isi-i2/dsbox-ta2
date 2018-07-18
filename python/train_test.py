b81e5f94ad7dba15e5790900e44e4e2e0eecbbdf
RqS <shaorunqi@gmail.com>
2018-07-17 17:32:13 -0700

    train_test takes in argv[1]: template, argv[2]: config dir

--- python/train_test.py
+++ python/train_test.py
@@ -1,25 +1,28 @@
 import os
 import subprocess
+import sys
 
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
 
+
 def call_ta2search(command):
-	p = subprocess.Popen(command, shell=True)
-	p.communicate()
+    p = subprocess.Popen(command, shell=True)
+    p.communicate()
+
 
 num = 5
 tp = ThreadPool(num)
 
 home = str(Path.home())
-config_dir = home + "/dsbox/runs2/config-ll0/"
+config_dir = sys.argv[2]
 
 for conf in os.listdir(config_dir):
-	command = "python ta2-search " + config_dir + conf
+    command = "python3 ta1-run-single-template --template " + sys.argv[1] + " " + config_dir + conf
 
-	print(command)
+    print(command)
 
-	tp.apply_async(call_ta2search, (command,))
+    tp.apply_async(call_ta2search, (command,))
 
 tp.close()
 tp.join()
