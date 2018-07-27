14dac2bc5ff0471ec2782e814bd028177c695e2d
RUNQI SHAO <shaorunqi@gmail.com>
2018-07-27 14:34:17 -0700

    Update controller.py

--- python/dsbox/controller/controller.py
+++ python/dsbox/controller/controller.py
@@ -516,7 +516,8 @@ class Controller:
 
         # print("[INFO] Choices:", choices)
         # UCT based evaluation
-        for i in range(max_iter):
+#         for i in range(max_iter):
+        while True:
             valids = list(filter(lambda t: t[1] is not None,
                                  zip(choices, self.uct_score)))
             _choices = list(map(lambda t: t[0], valids))
