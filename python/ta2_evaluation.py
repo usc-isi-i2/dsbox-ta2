2df3859cd7abb579cd58d6625d2b6373d7411f19
Ke-Thia Yao <kyao@isi.edu>
2018-08-07 14:35:28 -0700

    Update with 3 minute cleanup before timeout

--- python/ta2_evaluation.py
+++ python/ta2_evaluation.py
@@ -1,5 +1,4 @@
 import time
-start_time = time.clock()
 import json
 import os
 import signal
@@ -9,6 +8,7 @@ from pprint import pprint
 from dsbox.controller.controller import Controller
 from dsbox.controller.controller import Status
 
+start_time = time.time()
 
 def main():
     timeout = 0
@@ -19,45 +19,45 @@ def main():
 
     config["cpus"] = os.environ["D3MCPU"]
     config["ram"] = os.environ["D3MRAM"]
-    config["timeout"] = os.environ["D3MTIMEOUT"]
+
+    # Time to write results (in minutes)
+    write_results_time = 3
+    timeout = int(os.environ["D3MTIMEOUT"]) - write_results_time
+    config["timeout"] = timeout
 
     controller = Controller(development_mode=False)
 
     # Define signal handler to exit gracefully
-    # Either on an interrupt or after a certain time
     def write_results_and_exit(a_signal, frame):
-        # print('SIGNAL exit: {}'.format(configuration_file))
+        print('==== Times up ====')
+        time_used = (time.time() - start_time) / 60.0
+        print("[INFO] The time used so far is {:0.2f} minutes.".format(time_used))
         try:
             # Reset to handlers to default as not to output multiple times
-            signal.signal(signal.SIGINT, signal.SIG_DFL)
-            signal.signal(signal.SIGTERM, signal.SIG_DFL)
             signal.signal(signal.SIGALRM, signal.SIG_DFL)
-            # print('SIGNAL exit done reset signal {}'.format(configuration_file))
 
             controller.write_training_results()
-            # print('SIGNAL exit done writing: {}'.format(
-            #     configuration_file), flush=True)
+            print('==== Done cleaning up ====')
+            time_used = (time.time() - start_time) / 60.0
+            print("[INFO] The time used so far is {:0.2f} minutes.".format(time_used))
         except Exception as e:
             print(e)
             traceback.print_exc()
         finally:
             # sys.exit(0) generates SystemExit exception, which may
-            # be caught and ignore.
+            # be caught and ignored.
 
             # This os._exit() cannot be caught.
             # print('SIGNAL exiting {}'.format(configuration_file), flush=True)
             os._exit(0)
 
-    # Set timeout, alarm and signal handler
-    if 'timeout' in config:
-        # Timeout less 1 minute to give system chance to clean up
-        timeout = int(config['timeout']) - 1
+
+
     if timeout > 0:
-        signal.signal(signal.SIGINT, write_results_and_exit)
-        signal.signal(signal.SIGTERM, write_results_and_exit)
         signal.signal(signal.SIGALRM, write_results_and_exit)
         signal.alarm(60 * timeout)
-    config['timeout'] = timeout
+    else:
+        raise Exception('Negative timeout {}'.format(timeout))
 
     print('Using configuation:')
     pprint(config)
@@ -78,8 +78,9 @@ def main():
         status = Status.PROBLEM_NOT_IMPLEMENT
         print("[ERROR] Neither train or test root was given, the program will exit.")
 
-    time_used = time.clock() - start_time
-    print("[INFO] The time used for running program is", time_used, "seconds.")
+    time_used = (time.time() - start_time) / 60.0
+    print("[INFO] The time used for running program is {:0.2f} minutes.".format(time_used))
+
     return status.value
 
 
