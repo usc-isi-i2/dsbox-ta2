22afa16c3d820ca616debe37eaa1c7c37da11d03
Serban Stan <serbanandrei.stan@gmail.com>
2018-08-08 17:33:27 -0700

    more tinkering with hyperparams

--- python/dsbox/template/library.py
+++ python/dsbox/template/library.py
@@ -366,7 +366,7 @@ def dsbox_generic_steps(data : str = "data", target : str = "target"):
                     "primitive": "d3m.primitives.sklearn_wrap.SKPCA",
                     "hyperparameters":
                     {
-                        'n_components': [10, 15]
+                        'n_components': [10, 15, 25]
                     }
                 },
                 "d3m.primitives.dsbox.DoNothing",
@@ -588,7 +588,7 @@ def dsbox_feature_selector(ptype):
                     {
                         "primitive" : "d3m.primitives.sklearn_wrap.SKSelectFwe",
                         "hyperparameters":{
-                            "alpha" : [float(x) for x in np.logspace(-4, -1, 5)]
+                            "alpha" : [float(x) for x in np.logspace(-4, -1, 6)]
                         }
                     },
 
@@ -597,7 +597,7 @@ def dsbox_feature_selector(ptype):
                         "hyperparameters":{
                             "score_func": ["f_regression"],
                             "mode" : ["percentile"],
-                            "param" : [int(x) for x in np.linspace(10, 100, 10)]
+                            "param" : [5, 7, 10, 15, 30, 50, 75],
                         }
                     },
                     "d3m.primitives.dsbox.DoNothing"
@@ -615,7 +615,7 @@ def dsbox_feature_selector(ptype):
                     {
                         "primitive" : "d3m.primitives.sklearn_wrap.SKSelectFwe",
                         "hyperparameters":{
-                            "alpha" : [float(x) for x in np.logspace(-4, -1, 5)]
+                            "alpha" : [float(x) for x in np.logspace(-4, -1, 6)]
                         }
                     },
 
@@ -623,7 +623,7 @@ def dsbox_feature_selector(ptype):
                         "primitive" : "d3m.primitives.sklearn_wrap.SKGenericUnivariateSelect",
                         "hyperparameters":{
                             "mode" : ["percentile"],
-                            "param" : [int(x) for x in np.linspace(10, 100, 10)]
+                            "param" : [5, 7, 10, 15, 30, 50, 75],
                         }
                     },
                     "d3m.primitives.dsbox.DoNothing"
