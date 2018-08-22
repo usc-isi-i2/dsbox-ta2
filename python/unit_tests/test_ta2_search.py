96447cc7b3804c35a6ae48c0d6bf85fbeca85011
RqS <shaorunqi@gmail.com>
2018-08-21 17:59:56 -0700

    unit test

--- python/unit_tests/test_ta2_search.py
+++ python/unit_tests/test_ta2_search.py
@@ -7,9 +7,9 @@ import os
 class TestTA2Search(unittest.TestCase):
 
     def setUp(self):
-        self.search_config = json.load(open("resources/38_sick/search_config.json", 'r'))
+        self.dirname = os.path.dirname(__file__)
+        self.search_config = json.load(open(os.path.join(self.dirname, "resources/38_sick/search_config.json"), 'r'))
         self.output_root = os.path.dirname(self.search_config["pipeline_logs_root"])
-        print(self.search_config, self.output_root)
 
     def test_search_result_layout(self):
         keys = ["pipeline_logs_root", "executables_root", "temp_storage_root"]
