1ba65bdc21e931fb78bdec2c4fb5c90181e82dec
RqS <shaorunqi@gmail.com>
2018-08-21 17:26:13 -0700

    test search

--- python/unit_tests/test_ta2_search.py
+++ python/unit_tests/test_ta2_search.py
@@ -8,4 +8,10 @@ class TestTA2Search(unittest.TestCase):
 
     def setUp(self):
         self.search_config = json.load(open("resources/38_sick/search_config.json", 'r'))
-        print(self.search_config)
+        self.output_root = os.path.dirname(self.search_config["pipeline_logs_root"])
+        print(self.search_config, self.output_root)
+
+    def test_search_result_layout(self):
+        keys = ["pipeline_logs_root", "executables_root", "temp_storage_root"]
+        for key in keys:
+            self.assertEqual(os.path.exists(self.search_config[key]), True, "{} not exist".format(key))
