aee0132f6e898ab6fa74bca56ddf35a6f0f77174
liangmuxin <1441793911@qq.com>
2019-02-14 15:44:54 -0800

    d3mindex for image data, pca hyperparams

--- python/dsbox/template/library.py
+++ python/dsbox/template/library.py
@@ -85,8 +85,6 @@ class TemplateLibrary:
             #"Default_timeseries_collection_template": DefaultTimeseriesCollectionTemplate,
             "Default_image_processing_regression_template":
                 DefaultImageProcessingRegressionTemplate,
-            "TA1DefaultImageProcessingRegressionTemplate":
-                TA1DefaultImageProcessingRegressionTemplate,
             "TA1_classification_template_1": TA1ClassificationTemplate1,
             "MuxinTA1ClassificationTemplate1": MuxinTA1ClassificationTemplate1,
             "MuxinTA1ClassificationTemplate2": MuxinTA1ClassificationTemplate2,
@@ -1998,12 +1996,6 @@ class TA1VggImageProcessingRegressionTemplate(DSBoxTemplate):
                     }],
                     "inputs": ["to_dataframe_step"]
                 },
-                # {
-                #     "name": "column_parser_step",
-                #     "primitives": ["d3m.primitives.data_transformation.column_parser.DataFrameCommon"],
-                #     "inputs": ["to_dataframe_step"]
-                # },
-                # read X value
                 {
                     "name": "dataframe_to_tensor",
                     "primitives": ["d3m.primitives.data_preprocessing.DataFrameToTensor.DSBOX"],
@@ -2011,94 +2003,27 @@ class TA1VggImageProcessingRegressionTemplate(DSBoxTemplate):
                 },
                 {
                     "name": "feature_extraction",
-                    # "primitives": ["d3m.primitives.feature_extraction.ResNet50ImageFeature.DSBOX"],
-                    "primitives": ["d3m.primitives.feature_extraction.Vgg16ImageFeature.DSBOX"],
-                    "inputs": ["dataframe_to_tensor"]
-                },
-                {
-                    "name": "PCA_step",
-                    "primitives": ["d3m.primitives.data_transformation.pca.SKlearn"],
-                    "inputs": ["feature_extraction"]
-                },
-
-                {
-                    "name": "regressor_step",
                     "primitives": [
                         {
-                            "primitive": "d3m.primitives.regression.random_forest.SKlearn",
+                            "primitive": "d3m.primitives.feature_extraction.Vgg16ImageFeature.DSBOX",
                             "hyperparameters": {
-                                'add_index_columns': [True],
+                                'generate_metadata': [True]
                             }
                         }
                     ],
-                    "inputs": ["PCA_step", "extract_target_step"]
-                },
-            ]
-        }
-
-    def importance(datset, problem_description):
-        return 7
-
-
-class TA1DefaultImageProcessingRegressionTemplate(DSBoxTemplate):
-    def __init__(self):
-        DSBoxTemplate.__init__(self)
-        self.template = {
-            "name": "Default_image_processing_regression_template",
-            "taskType": TaskType.REGRESSION.name,
-            # See TaskType, range include 'CLASSIFICATION', 'CLUSTERING', 'COLLABORATIVE_FILTERING',
-            # 'COMMUNITY_DETECTION', 'GRAPH_CLUSTERING', 'GRAPH_MATCHING', 'LINK_PREDICTION',
-            # 'REGRESSION', 'TIME_SERIES_FORECASTING', 'VERTEX_NOMINATION'
-            "taskSubtype": {TaskSubtype.UNIVARIATE.name, TaskSubtype.MULTIVARIATE.name},
-            "inputType": "image",  # See SEMANTIC_TYPES.keys() for range of values
-            "output": "regressor_step",  # Name of the final step generating the prediction
-            "target": "extract_target_step",  # Name of the step generating the ground truth
-            "steps": [
-
-                {
-                    "name": "denormalize_step",
-                    "primitives": ["d3m.primitives.normalization.Denormalize.DSBOX"],
-                    "inputs": ["template_input"]
-                },
-                {
-                    "name": "to_dataframe_step",
-                    "primitives": ["d3m.primitives.data_transformation.dataset_to_dataframe.Common"],
-                    "inputs": ["denormalize_step"]
-                },
-                # read Y value
-                {
-                    "name": "extract_target_step",
-                    "primitives": [{
-                        "primitive": "d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon",
-                        "hyperparameters":
-                            {
-                                'semantic_types': ('https://metadata.datadrivendiscovery.org/types/TrueTarget',),
-                                'use_columns': (),
-                                'exclude_columns': ()
-                            }
-                    }],
-                    "inputs": ["to_dataframe_step"]
-                },
-                # {
-                #     "name": "column_parser_step",
-                #     "primitives": ["d3m.primitives.data_transformation.column_parser.DataFrameCommon"],
-                #     "inputs": ["to_dataframe_step"]
-                # },
-                # read X value
-                {
-                    "name": "dataframe_to_tensor",
-                    "primitives": ["d3m.primitives.data_preprocessing.DataFrameToTensor.DSBOX"],
-                    "inputs": ["to_dataframe_step"]
-                },
-                {
-                    "name": "feature_extraction",
-                    "primitives": ["d3m.primitives.feature_extraction.ResNet50ImageFeature.DSBOX"],
-                    # "primitives": ["d3m.primitives.feature_extraction.Vgg16ImageFeature.DSBOX"],
                     "inputs": ["dataframe_to_tensor"]
                 },
                 {
                     "name": "PCA_step",
-                    "primitives": ["d3m.primitives.data_transformation.pca.SKlearn"],
+                    "primitives": [
+                        {
+                            "primitive": "d3m.primitives.data_transformation.pca.SKlearn",
+                            "hyperparameters": {
+                                'add_index_columns': [True],
+                                'use_semantic_types':[True]
+                            }
+                        }
+                    ],
                     "inputs": ["feature_extraction"]
                 },
 
@@ -2171,12 +2096,27 @@ class DefaultImageProcessingRegressionTemplate(DSBoxTemplate):
                 },
                 {
                     "name": "feature_extraction",
-                    "primitives": ["d3m.primitives.feature_extraction.ResNet50ImageFeature.DSBOX"],
+                    "primitives": [
+                        {
+                            "primitive": "d3m.primitives.feature_extraction.ResNet50ImageFeature.DSBOX",
+                            "hyperparameters": {
+                                'generate_metadata': [True]
+                            }
+                        }
+                    ],
                     "inputs": ["dataframe_to_tensor"]
                 },
                 {
                     "name": "PCA_step",
-                    "primitives": ["d3m.primitives.data_transformation.pca.SKlearn"],
+                    "primitives": [
+                        {
+                            "primitive": "d3m.primitives.data_transformation.pca.SKlearn",
+                            "hyperparameters": {
+                                'add_index_columns': [True],
+                                'use_semantic_types':[True]
+                            }
+                        }
+                    ],
                     "inputs": ["feature_extraction"]
                 },
 
