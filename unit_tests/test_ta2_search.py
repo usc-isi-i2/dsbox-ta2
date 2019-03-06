b86b3c7a8032530d43b3017eb9845235b7c62a1e
Ke-Thia Yao <kyao@isi.edu>
2019-03-05 19:41:31 -0800

    update travis

--- unit_tests/test_ta2_search.py
+++ unit_tests/test_ta2_search.py
@@ -1,70 +1,80 @@
-import unittest
-import sys
 import json
 import os
+import pathlib
+import unittest
 
 
 class TestTA2Search(unittest.TestCase):
 
     def setUp(self):
-        self.dirname = os.path.dirname(__file__)
-        self.search_config = json.load(open(os.path.join(self.dirname, "resources/38_sick/search_config.json"), 'r'))
-        self.output_root = os.path.dirname(self.search_config["pipeline_logs_root"])
+        # self.output_root_dir = pathlib.Path.home() / 'output'
+        # self.output_root_dir = pathlib.Path('/nas/home/kyao/output/docker/seed/uu1_datasmash')
+        self.output_root_dir = pathlib.Path('/nas/home/kyao/output/docker/seed/38_sick')
+        self.pipelines_scored_dir = self.output_root_dir / 'pipelines_scored'
+        self.pipelines_ranked_dir = self.output_root_dir / 'pipelines_ranked'
+        self.predictions_dir = self.output_root_dir / 'predictions'
+        self.scores_dir = self.output_root_dir / 'score'
 
-        self.pipelines_dir = self.search_config["pipeline_logs_root"]
-        self.pipelines_considered_dir = os.path.join(self.output_root, "pipelines_considered")
-        self.executables_dir = self.search_config["executables_root"]
-        self.supporting_files_dir = self.search_config["temp_storage_root"]
-
-        self.executables_files = [x for x in os.listdir(self.executables_dir) if
-                                  os.path.isfile(os.path.join(self.executables_dir, x))]
-        self.pipeline_files = [x for x in os.listdir(self.pipelines_dir) if
-                               os.path.isfile(os.path.join(self.pipelines_dir, x))]
-        self.pickled_dir = [x for x in os.listdir(self.supporting_files_dir) if
-                            os.path.isdir(os.path.join(self.supporting_files_dir, x)) and x != "logs"]
+        self.scored_pipelines = list(self.pipelines_scored_dir.glob('*.json'))
+        self.ranked_pipelines = list(self.pipelines_ranked_dir.glob('*.json'))
+        self.predictions = list(self.predictions_dir.glob('*.csv'))
+        self.scores = list(self.scores_dir.glob('*.score.json'))
 
     @staticmethod
     def _valid_json(file_name):
         try:
-            json.load(open(file_name, 'r'))
-        except:
+            with open(file_name, 'r') as f:
+                json.load(f)
+        except Exception:
             return False
         return True
 
     def test_search_result_layout(self):
-        keys = ["pipeline_logs_root", "executables_root", "temp_storage_root"]
-        for key in keys:
-            self.assertEqual(os.path.exists(self.search_config[key]), True, "{} not exist".format(key))
+        dirs = ["pipelines_ranked", "pipelines_scored", "predictions", "score", "pipelines_searched"]
+        for dir in dirs:
+            self.assertEqual(os.path.exists(self.output_root_dir / dir), True, "{} not exist".format(dir))
 
-    def test_pipeline_generated(self):
-        self.assertGreater(len(self.pipeline_files), 0, "No pipeline generated")
+    def test_pipeline_scored(self):
+        self.assertGreater(len(self.scored_pipelines), 0, "No pipeline scored")
 
-    def test_pipelines_dir_split(self):
-        if os.path.exists(self.pipelines_considered_dir):
-            self.assertLessEqual(len(self.pipeline_files), 20, "pipelines more than 20 in /pipelines")
+    def test_pipeline_ranked(self):
+        self.assertGreater(len(self.ranked_pipelines), 0, "No pipeline ranked")
 
-    def test_pipelines_and_executables_dirs(self):
-        self.assertEqual(self.executables_files, self.pipeline_files, "pipelines and executables file name mismatch")
+    # def test_pipelines_dir_split(self):
+    #     if self.pipelines_ranked_dir.exists():
+    #         self.assertLessEqual(len(self.ranked_pipelines), 20, "More than 20 pipelines in /pipelines_ranked")
 
-    def test_pipelines_and_supporting_files_dir(self):
-        trimed_json_suffix = sorted([x[:-5] for x in self.pipeline_files])
-        self.assertEqual(sorted(self.pickled_dir), trimed_json_suffix, "pipelines and supporting files name mismatch")
+    def test_predictions(self):
+        self.assertGreater(len(self.predictions), 0, "No predictions generated")
+        if self.predictions_dir.exists():
+            self.assertEqual(len(self.ranked_pipelines), len(self.predictions), "Some pipelines failed to generate predictions")
+        for file in self.predictions:
+            self.assertGreater((self.predictions_dir / file).stat().st_size, 0, f"Prediction file is empty: {file}")
 
-    def test_pipelines_valid(self):
-        for pipeline_file_name in self.pipeline_files:
-            self.assertEqual(self._valid_json(os.path.join(self.pipelines_dir, pipeline_file_name)), True,
+    def test_scores(self):
+        self.assertGreater(len(self.scores), 0, "No scores generated")
+        if self.scores_dir.exists():
+            self.assertEqual(len(self.ranked_pipelines), len(self.scores), "Some pipelines failed to generate scores")
+        for file in self.scores:
+            self.assertTrue((self.scores_dir / file).stat().st_size > 0, f"Prediction file is empty: {file}")
+
+    def test_pipelines_scored_valid(self):
+        for pipeline_file_name in self.scored_pipelines:
+            self.assertEqual(self._valid_json(self.pipelines_scored_dir / pipeline_file_name), True,
                              "{} pipeline json invalid".format(pipeline_file_name))
 
-    def test_pipelines_considered_valid(self):
-        if os.path.exists(self.pipelines_considered_dir):
-            for pipeline_file_name in os.listdir(self.pipelines_considered_dir):
-                if os.path.isfile(os.path.join(self.pipelines_considered_dir, pipeline_file_name)):
-                    self.assertEqual(self._valid_json(os.path.join(self.pipelines_considered_dir, pipeline_file_name)),
-                                     True,
-                                     "{} considered pipeline json invalid".format(pipeline_file_name))
+    def test_pipelines_ranked_valid(self):
+        for pipeline_file_name in self.ranked_pipelines:
+            self.assertEqual(self._valid_json(self.pipelines_ranked_dir / pipeline_file_name), True,
+                             "{} pipeline json invalid".format(pipeline_file_name))
 
     def test_pipeline_id(self):
-        for pipeline_file_name in self.pipeline_files:
-            obj = json.load(open(os.path.join(self.pipelines_dir, pipeline_file_name), 'r'))
-            self.assertEqual(pipeline_file_name[:-5], obj["id"],
+        for pipeline_file_name in self.scored_pipelines:
+            with open((self.pipelines_scored_dir / pipeline_file_name), 'r') as f:
+                obj = json.load(f)
+            self.assertEqual(pipeline_file_name.stem, obj["id"],
                              "{} pipeline file name and id mismatch".format(pipeline_file_name))
+
+
+if __name__ == '__main__':
+    unittest.main()
