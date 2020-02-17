5e08fb7e381b57520ff5eb9a8810917d12dc0a37
ckxz105 <liu494@usc.edu>
2020-02-16 18:09:37 -0800

    add CF template

--- python/dsbox/template/template_files/loaded/DefaultCollaborativeFilteringTabularTemplate.py
+++ python/dsbox/template/template_files/loaded/DefaultCollaborativeFilteringTabularTemplate.py
@@ -11,13 +11,12 @@ class DefaultCollaborativeFilteringTabularTemplate(DSBoxTemplate):
     def __init__(self):
         DSBoxTemplate.__init__(self)
         self.template = {
-            "weight": 100,
             "name": "Default_Collaborative_Filtering_Tabular_Template",
             "taskType": {TaskKeyword.COLLABORATIVE_FILTERING.name},
             # for some special condition, the taskSubtype can be "NONE" which indicate no taskSubtype given
             "taskSubtype":  {TaskKeyword.COLLABORATIVE_FILTERING.name},
             "inputType": {"table"},
-            "output": "model_step",
+            "output": "construct_predictions_step",
             "steps": [
                 {
                     "name": "to_dataframe_step",
@@ -30,18 +29,33 @@ class DefaultCollaborativeFilteringTabularTemplate(DSBoxTemplate):
                     "inputs": ["to_dataframe_step"]
                 },
                 {
+                    "name": "parser_step",
+                    "primitives": [{
+                        "primitive": "d3m.primitives.data_transformation.column_parser.Common",
+                        "hyperparameters":
+                            {
+                                'parse_semantic_types': ([
+                                    "http://schema.org/Boolean",
+                                    "http://schema.org/Integer",
+                                    "http://schema.org/Float",
+                                    "https://metadata.datadrivendiscovery.org/types/FloatVector"
+                                  ],),
+                            }
+                    }],
+                    "inputs": ["common_profiler_step"]
+                },
+                {
                     "name": "extract_attribute_step",
                     "primitives": [{
                         "primitive": "d3m.primitives.data_transformation.extract_columns_by_semantic_types.Common",
                         "hyperparameters":
                             {
-                                'semantic_types': ('https://metadata.datadrivendiscovery.org/types/PrimaryKey',
-                                                   'https://metadata.datadrivendiscovery.org/types/Attribute',),
+                                'semantic_types': (['https://metadata.datadrivendiscovery.org/types/Attribute'],),
                                 'use_columns': (),
                                 'exclude_columns': ()
                             }
                     }],
-                    "inputs": ["common_profiler_step"]
+                    "inputs": ["parser_step"]
                 },
                 {
                     "name": "to_numeric_step",
@@ -59,25 +73,25 @@ class DefaultCollaborativeFilteringTabularTemplate(DSBoxTemplate):
                                 'exclude_columns': ()
                             }
                     }],
-                    "inputs": ["common_profiler_step"]
+                    "inputs": ["parser_step"]
                 },
                 {
                     "name": "model_step",
                     "primitives": [{
-                        "primitive": "d3m.primitives.classification.random_forest.SKlearn",
-                        "hyperparameters": {
-                            # 'bootstrap': ["bootstrap", "disabled"],
-                            'max_depth': [15, 30, None],
-                            'min_samples_leaf': [1, 2, 4],
-                            'min_samples_split': [2, 5, 10],
-                            'max_features': ['auto', 'sqrt'],
-                            'n_estimators': [10, 50, 100],
-                            'add_index_columns': [True],
-                            'use_semantic_types':[True],
-                        }
+                        "primitive": "d3m.primitives.collaborative_filtering.link_prediction.DistilCollaborativeFiltering"
+                        "hyperparameters": {}
                     }
                     ],
                     "inputs": ["to_numeric_step", "extract_target_step"]
+                }
+                {
+                    "name": "construct_predictions_step",
+                    "primitives": [{
+                        "primitive": "d3m.primitives.data_transformation.construct_predictions.Common"
+                        "hyperparameters": {}
+                    }
+                    ],
+                    "inputs": ["model_step", "common_profiler_step"]
                 }
             ]
         }
